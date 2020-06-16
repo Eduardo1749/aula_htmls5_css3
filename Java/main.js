280c0088b087b99e7da3bdb71927d42255fff5f2
Eduardo <dudu.evangelista1749@gmail.com>
2020-06-16 17:52:55 -0300

    Adicionando o JS

---
+++ Java/main.js
@@ -0,0 +1,9 @@
+function clicar(){
+    alert("Você ira para o canal no Youtube desse proplayer");
+}
+
+function mause(){
+    alert("Se você clicar na imagem você ira para o canal desse proplayer");
+}
+
+
